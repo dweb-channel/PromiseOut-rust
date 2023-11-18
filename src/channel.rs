7b70f37741598cef74bda22e5f6001166fdafeaa
Shane Celis <shane.celis@gmail.com>
2023-11-18 06:45:33 -0500

    feature: Finish mpsc channel promise.

--- src/channel.rs
+++ src/channel.rs
@@ -1,3 +1,6 @@
+//! A channel promise uses a multi-producer, single-consumer channel as its
+//! backend. This allows for the Producer to be cloned but not the Consumer.
+//!
 use std::{
     future::Future, task::{Poll, Waker},
     sync::{
@@ -5,15 +8,22 @@ use std::{
         Arc, Mutex,
     }
 };
-use crate::{Promise, Error};
+use crate::{Promise, Error, WakerState};
 #[derive(Debug, Clone)]
 pub struct Producer<T> {
     sender: Sender<T>,
+    promise: Arc<Mutex<Inner>>,
 }
 
 #[derive(Debug)]
 pub struct Consumer<T> {
-    receiver: Receiver<T>
+    receiver: Receiver<T>,
+    promise: Arc<Mutex<Inner>>,
+}
+
+#[derive(Debug)]
+struct Inner {
+    waker: Result<Waker, WakerState>,
 }
 
 impl<T> Future for Consumer<T> {
@@ -25,40 +35,34 @@ impl<T> Future for Consumer<T> {
     ) -> std::task::Poll<Self::Output> {
         match self.receiver.try_recv() {
             Ok(value) => Poll::Ready(Ok(value)),
-            Err(TryRecvError::Empty) => Poll::Pending,
+            Err(TryRecvError::Empty) => {
+                let mut promise = self.promise.lock().unwrap();
+                match std::mem::replace(&mut promise.waker, Ok(cx.waker().clone())) {
+                    Err(WakerState::Tainted) => Poll::Ready(Err(Error::ProducerDropped)),
+                    _ => Poll::Pending
+                }
+            }
             Err(TryRecvError::Disconnected) => Poll::Ready(Err(Error::ProducerDropped))
         }
     }
 }
 
-// impl<T> Clone for Producer<T> {
-//     fn clone(&self) -> Self {
-//         Self {
-//             sender: self.sender.clone(),
-//             receiver: self.receiver.clone(),
-//         }
-//     }
-// }
-
-// impl<T> Default for Producer<T> {
-//     fn default() -> Self {
-//         let (tx, rx) = channel();
-//         Self {
-//             sender: tx,
-//             receiver: rx,
-//         }
-//     }
-// }
-
 impl<T> Promise<T> for Producer<T> {
     type Waiter = Consumer<T>;
     fn resolve(self, value: T) {
         self.sender.send(value).unwrap();
+        let mut promise = self.promise.lock().unwrap();
+        if let Ok(waker) = std::mem::replace(&mut promise.waker, Err(WakerState::Tainted)) {
+            waker.wake()
+        }
     }
 
-    fn new() -> (Self, Self::Waiter) where Self: Sized{
+    fn new() -> (Self, Self::Waiter) where Self: Sized {
         let (tx, rx) = channel();
-        (Self {sender: tx },
-         Consumer { receiver: rx })
+        let inner = Arc::new(Mutex::new(Inner {
+                waker: Err(WakerState::Fresh),
+            }));
+        (Producer { sender: tx,   promise: inner.clone() },
+         Consumer { receiver: rx, promise:  inner })
     }
 }
